d951143d7e1bfa234b4cd43fde2d42df3ba546b6
Ana Carolina <84293579+AyaCarol@users.noreply.github.com>
2024-09-29 23:38:24 -0300

    Adicionado calculo do salario + comissão

--- src/main/java/modulobonus/Vendedor.java
+++ src/main/java/modulobonus/Vendedor.java
@@ -1,31 +1,36 @@
 package modulobonus;
 
+import java.math.BigDecimal;
+
 public class Vendedor extends Funcionario{
     private double comissao;
-    private int vendas;
+    private double vendas;
     //private BigDecimal vendas;
 
-    public Vendedor(int id, double salario, Departamento departamento, double comissao, int vendas) {
+    public Vendedor(int id, double salario, Departamento departamento, double comissao, double vendas) {
         super(id, salario, Cargo.VENDEDOR, departamento);
         this.comissao = comissao;
         this.vendas = vendas;
+        
     }
 
-    public int getVendas() {
+    public double getVendas() {
         return vendas;
     } 
     
-    /*
+    
     public void calcularComissao(){
-        this.comissao = this.vendas.multiply(BigDecimal.valueOf(0.01));
+        this.comissao = vendas * 0.01;
+        //this.comissao = this.vendas.multiply(BigDecimal.valueOf(0.01));
     
     }
     
-    melhor usar BC pois nao acontece erro de arredondamento como em double
+    public void calcularSalario(){
+        this.salario += this.comissao;
+        //this.salario = this.salario.add(this.comissao);
+    }
     
+    //melhor usar BC pois nao acontece erro de arredondamento como em double
     
-    */
-    
-    
-    
+
 }
