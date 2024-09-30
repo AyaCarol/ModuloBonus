05ea305cf5b7e6ec98bbb67699d4dd7262b4e160
Ana Carolina <84293579+AyaCarol@users.noreply.github.com>
2024-09-29 21:49:31 -0300

    Alterações na classe Vendedor

--- src/main/java/modulobonus/Vendedor.java
+++ src/main/java/modulobonus/Vendedor.java
@@ -3,6 +3,7 @@ package modulobonus;
 public class Vendedor extends Funcionario{
     private double comissao;
     private int vendas;
+    //private BigDecimal vendas;
 
     public Vendedor(int id, double salario, Departamento departamento, double comissao, int vendas) {
         super(id, salario, Cargo.VENDEDOR, departamento);
@@ -13,4 +14,18 @@ public class Vendedor extends Funcionario{
     public int getVendas() {
         return vendas;
     } 
+    
+    /*
+    public void calcularComissao(){
+        this.comissao = this.vendas.multiply(BigDecimal.valueOf(0.01));
+    
+    }
+    
+    melhor usar BC pois nao acontece erro de arredondamento como em double
+    
+    
+    */
+    
+    
+    
 }
