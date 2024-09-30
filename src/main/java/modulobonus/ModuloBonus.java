1fe9068a97bf0f579261093f9f4a604a5c3f868e
Ana Carolina <84293579+AyaCarol@users.noreply.github.com>
2024-09-29 21:49:10 -0300

    Alterações na classe main

--- src/main/java/modulobonus/ModuloBonus.java
+++ src/main/java/modulobonus/ModuloBonus.java
@@ -7,9 +7,9 @@ public class ModuloBonus {
         Departamento vendas2 = new Departamento(2);
         
         Vendedor v1 = new Vendedor(1, 1200, vendas1, 120, 10);
-        Vendedor v2 = new Vendedor(1, 1300, vendas1, 120, 20);
-        Vendedor v3 = new Vendedor(1, 1200, vendas2, 120, 15);
-        Vendedor v4 = new Vendedor(1, 1200, vendas2, 120, 35);
+        Vendedor v2 = new Vendedor(2, 1300, vendas1, 120, 20);
+        Vendedor v3 = new Vendedor(3, 1200, vendas2, 120, 15);
+        Vendedor v4 = new Vendedor(4, 1200, vendas2, 120, 35);
         
         vendas1.adicionarFuncionario(v1);
         vendas1.adicionarFuncionario(v2);
@@ -23,6 +23,14 @@ public class ModuloBonus {
         
         System.out.println("O departamento com maior venda foi: " + controle.verificaMaiorVenda().getId());
         
-        
+         /*
+        - ADICIONAR CALCULO DE SALARIO + COMISSAO
+        - Modificar FOR por FOR EACH no Controle   FEITO!!!
+        - adicionar calculo de bonus
+        - adicionar verificação se é vendedor ou gerente e dar respectivos bonus
+        - adicionar tratativa de erro
+               Código 1: Retornado se qualquer uma das tabelas de entrada (Funcionários ou Departamentos) estiver vazia, ou seja, sem dados.
+               Código 2: Retornado se não houver nenhum funcionário elegível nos departamentos com maior valor de vendas.
+        */
     }
 }
