4ad208ffdefd5016918462e5c8d16e636fa10478
Ana Carolina <84293579+AyaCarol@users.noreply.github.com>
2024-10-04 12:00:43 -0300

    Alteração na classe Vendedor, valores monetários de double para BigDecimal

--- src/main/java/modulobonus/Vendedor.java
+++ src/main/java/modulobonus/Vendedor.java
@@ -3,35 +3,28 @@ package modulobonus;
 import java.math.BigDecimal;
 
 public class Vendedor extends Funcionario{
-    protected double comissao;
-    protected double vendas;
+    protected BigDecimal comissao;
+    protected BigDecimal vendas;
     //private BigDecimal vendas;
 
-    public Vendedor(int id, double salario, Departamento departamento, double comissao, double vendas) {
+    public Vendedor(int id, BigDecimal salario, Departamento departamento, BigDecimal vendas) {
         super(id, salario, Cargo.VENDEDOR, departamento);
-        this.comissao = comissao;
         this.vendas = vendas;
         
     }
 
-    public double getVendas() {
+    public BigDecimal getVendas() {
         return vendas;
     } 
     
-  
-    
     public void calcularSalario(){
-        this.comissao = vendas * 0.01;
-        this.salario += this.comissao;
-        //this.salario = this.salario.add(this.comissao);
+        this.comissao = this.vendas.multiply(BigDecimal.valueOf(0.01));
+        this.salario = this.salario.add(this.comissao);
     }
-    
     
     //melhor usar BC pois nao acontece erro de arredondamento como em double
 
-    public double getComissao() {
+    public BigDecimal getComissao() {
         return comissao;
     }
-    
-
 }
