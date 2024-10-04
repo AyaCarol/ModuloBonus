841c98e6e9f9630b54b402d01595079b7c9043bb
Ana Carolina <84293579+AyaCarol@users.noreply.github.com>
2024-10-04 12:01:13 -0300

    Alteração da classe Gerente, valores monetários de double para BigDecimal

--- src/main/java/modulobonus/Gerente.java
+++ src/main/java/modulobonus/Gerente.java
@@ -1,8 +1,10 @@
 package modulobonus;
 
+import java.math.BigDecimal;
+
 public class Gerente extends Funcionario{
     
-    public Gerente(int id, double salario, Departamento departamento) {
+    public Gerente(int id, BigDecimal salario, Departamento departamento) {
         super(id, salario, Cargo.GERENTE, departamento);
     }
     
